3be03ed18ed858d3d19ccc7e74e990e9e0855384
Furuta Naoki <naoki.furuta@japan-systems.co.jp>
2025-07-31 16:40:35 +0900

    feat: AnimationStarコンポーネントを追加

---
+++ src/components/AnimationStar.tsx
@@ -0,0 +1,45 @@
+export default function AnimationStar() {
+  return (
+    <div className="absolute inset-0 pointer-events-none">
+      {/* キラキラエフェクト - 花の画像内に収まるよう配置 */}
+      <div className="absolute top-36 left-28 w-3 h-3 bg-yellow-200 rounded-full animate-ping opacity-70 animation-delay-1300"></div>
+      <div className="absolute top-32 left-24 w-3 h-3 bg-yellow-200 rounded-full animate-bounce opacity-70 animation-delay-500"></div>
+      <div className="absolute top-16 left-20 w-3 h-3 bg-yellow-300 rounded-full animate-ping opacity-75"></div>
+      <div className="absolute top-7 left-40 w-3 h-3 bg-yellow-300 rounded-full animate-pulse opacity-75 animation-delay-900"></div>
+      <div className="absolute top-3 left-32 w-2 h-2 bg-yellow-400 rounded-full animate-pulse opacity-80 animation-delay-300"></div>
+      <div className="absolute top-28 left-64 w-2 h-2 bg-yellow-400 rounded-full animate-bounce opacity-80 animation-delay-1100"></div>
+      <div className="absolute top-40 left-36 w-2 h-2 bg-yellow-500 rounded-full animate-ping opacity-85 animation-delay-700"></div>
+      <div className="absolute top-44 left-60 w-2 h-2 bg-yellow-500 rounded-full animate-pulse opacity-85 animation-delay-1500"></div>
+
+      {/* 右側エリア */}
+      <div className="absolute top-4 left-60 w-3 h-3 bg-yellow-200 rounded-full animate-bounce opacity-70 animation-delay-1600"></div>
+      <div className="absolute top-44 left-10 w-2 h-2 bg-yellow-200 rounded-full animate-pulse opacity-70 animation-delay-800"></div>
+      <div className="absolute top-10 left-72 w-2 h-2 bg-yellow-300 rounded-full animate-bounce opacity-75 animation-delay-200"></div>
+      <div className="absolute top-12 left-60 w-2 h-2 bg-yellow-300 rounded-full animate-ping opacity-75 animation-delay-1200"></div>
+      <div className="absolute top-40 left-80 w-3 h-3 bg-yellow-400 rounded-full animate-ping opacity-80 animation-delay-600"></div>
+      <div className="absolute top-20 left-20 w-2 h-2 bg-yellow-400 rounded-full animate-pulse opacity-80 animation-delay-1400"></div>
+      <div className="absolute top-16 left-32 w-2 h-2 bg-yellow-500 rounded-full animate-ping opacity-85 animation-delay-1800"></div>
+      <div className="absolute top-80 left-12 w-3 h-3 bg-yellow-500 rounded-full animate-bounce opacity-85 animation-delay-1000"></div>
+
+      {/* 下側エリア */}
+      <div className="absolute top-56 left-28 w-2 h-2 bg-yellow-200 rounded-full animate-pulse opacity-70 animation-delay-750"></div>
+      <div className="absolute top-48 left-32 w-3 h-3 bg-yellow-200 rounded-full animate-ping opacity-70 animation-delay-350"></div>
+      <div className="absolute top-48 left-20 w-2 h-2 bg-yellow-300 rounded-full animate-pulse opacity-75 animation-delay-100"></div>
+      <div className="absolute top-12 left-14 w-2 h-2 bg-yellow-300 rounded-full animate-bounce opacity-75 animation-delay-550"></div>
+      <div className="absolute top-52 left-64 w-3 h-3 bg-yellow-400 rounded-full animate-ping opacity-80 animation-delay-650"></div>
+      <div className="absolute top-24 left-72 w-2 h-2 bg-yellow-400 rounded-full animate-bounce opacity-80 animation-delay-250"></div>
+      <div className="absolute top-14 left-40 w-2 h-2 bg-yellow-500 rounded-full animate-pulse opacity-85 animation-delay-450"></div>
+      <div className="absolute top-56 left-48 w-2 h-2 bg-yellow-500 rounded-full animate-bounce opacity-85 animation-delay-850"></div>
+
+      {/* 中央エリア */}
+      <div className="absolute top-44 left-14 w-3 h-3 bg-yellow-200 rounded-full animate-ping opacity-70 animation-delay-1550"></div>
+      <div className="absolute top-40 left-32 w-2 h-2 bg-yellow-200 rounded-full animate-bounce opacity-70 animation-delay-1150"></div>
+      <div className="absolute top-32 left-28 w-3 h-3 bg-yellow-300 rounded-full animate-ping opacity-75 animation-delay-950"></div>
+      <div className="absolute top-28 left-12 w-2 h-2 bg-yellow-300 rounded-full animate-pulse opacity-75 animation-delay-1350"></div>
+      <div className="absolute top-36 left-64 w-2 h-2 bg-yellow-400 rounded-full animate-pulse opacity-80 animation-delay-1050"></div>
+      <div className="absolute top-32 left-80 w-2 h-2 bg-yellow-400 rounded-full animate-bounce opacity-80 animation-delay-1450"></div>
+      <div className="absolute top-44 left-72 w-3 h-3 bg-yellow-500 rounded-full animate-ping opacity-85 animation-delay-1250"></div>
+      <div className="absolute top-44 left-80 w-2 h-2 bg-yellow-500 rounded-full animate-pulse opacity-85 animation-delay-1650"></div>
+    </div>
+  );
+}
