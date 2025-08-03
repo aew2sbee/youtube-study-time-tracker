36d4288cdd4b92e957290bb3b7f243b5a82fd653
aew2sbee <aew2sbee@gmail.com>
2025-08-03 12:29:21 +0900

    feat: HowToUseコンポーネントの内容を更新し、参加方法を明確化

--- src/components/HowToUse.tsx
+++ src/components/HowToUse.tsx
@@ -1,14 +1,13 @@
 export default function HowToUse() {
   return (
-    <div className="text-white text-center text-2xl flex-1 flex items-start justify-center pt-16">
+    <div className="text-white text-3xl items-start justify-start pt-4">
+      <p>誰でも気軽に参加できます 😊</p>
+      <p>※見てるだけでも、あいさつだけでも大歓迎です！</p>
+      <p className="pt-3">📌 参加方法</p>
       <div className="space-y-2">
-        <div>どなたでも時間の計測に参加できます</div>
-        <div>
-          コメント欄に<strong>「start」</strong>で開始、<strong>「end」</strong>で終了
-        </div>
-        <div>
-          複数回の<strong>「start」/「end」</strong>で時間が累積されます
-        </div>
+        <li>コメント欄に<strong>「start」</strong>と入力 → 計測<strong>開始</strong></li>
+        <li>コメント欄に<strong>「end」 </strong>と入力 → 計測<strong>終了</strong></li>
+        <li>何回でも参加OK。時間は自動で累積されます</li>
       </div>
     </div>
   );
