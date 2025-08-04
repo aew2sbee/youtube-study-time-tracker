38114bef27daf185053144383d03d8161201ebf2
Furuta Naoki <naoki.furuta@japan-systems.co.jp>
2025-08-04 14:04:15 +0900

    fix: ExplanationCrownコンポーネントの画像パスを更新

--- src/components/ExplanationCrown.tsx
+++ src/components/ExplanationCrown.tsx
@@ -5,11 +5,11 @@ export default function ExplanationCrown() {
     <div className="mt-4 p-3 bg-white/10 rounded-lg">
       <div className="flex justify-center items-center space-x-6">
         <div className="flex items-center space-x-2">
-          <Image src="/crown/crown_gold.png" alt="Gold Crown" width={24} height={24} className="w-6 h-6" />
+          <Image src="/crown/gold.png" alt="Gold Crown" width={24} height={24} className="w-6 h-6" />
           <span className="text-white text-xl">60min+</span>
         </div>
         <div className="flex items-center space-x-2">
-          <Image src="/crown/crown_silver.png" alt="Silver Crown" width={24} height={24} className="w-6 h-6" />
+          <Image src="/crown/silver.png" alt="Silver Crown" width={24} height={24} className="w-6 h-6" />
           <span className="text-white text-xl">30min+</span>
         </div>
       </div>
