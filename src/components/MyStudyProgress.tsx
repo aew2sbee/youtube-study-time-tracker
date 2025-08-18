1ea4635a598571df096f66b56713b5a84892c42f
aew2sbee <aew2sbee@gmail.com>
2025-08-18 22:33:45 +0900

    feat: 勉強進捗の更新時間と追加の勉強時間を修正

--- src/components/MyStudyProgress.tsx
+++ src/components/MyStudyProgress.tsx
@@ -1,7 +1,7 @@
 const studyProgressItems = [
-  { key: 'Update Time', text: '2025/08/17' },
+  { key: 'Update Time', text: '2025/08/18' },
   { key: 'Current Study', text: '基本情報技術者試験 (FE)' },
-  { key: 'Total Study Time', text: '40 / 150h' },
+  { key: 'Total Study Time', text: '41 / 150h' },
   { key: 'Exam Date', text: 'Not scheduled yet' },
 ];
 
