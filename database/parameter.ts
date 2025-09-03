150123c93e411a33843c533f3e02b9f7504fe0ad
aew2sbee <aew2sbee@gmail.com>
2025-09-03 16:44:32 +0900

    削除: parameter.tsとsqlite.dbファイルを削除

--- database/parameter.ts
+++
@@ -1,9 +0,0 @@
-// 追加の勉強時間（秒: h * m + sec）- 1時間
-export const ADDITIONAL_STUDY_TIME = 0 * 60 * 60;
-
-export const STUDY_PROGRESS_ITEMS = [
-  { key: 'Update Time', text: 'YYYY/MM/DD' },
-  { key: 'Current Study', text: '学習内容' },
-  { key: 'Total Study Time', text: '勉強時間' },
-  { key: 'Exam Date', text: '受験日' },
-];
