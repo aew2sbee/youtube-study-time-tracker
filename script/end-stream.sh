fd7971aaa7a0b39e988b6f4da5d2820668448bc7
Furuta Naoki <naoki.furuta@japan-systems.co.jp>
2025-09-22 10:28:47 +0900

    refactor: 不要なスクリプトファイルを削除

--- script/end-stream.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker-compose down
-npx tsx script/query-by-video-id.ts
--- script/start-stream.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-docker compose up -d --build
-npx tsx script/validate-refresh-token.ts
-docker compose logs -f
