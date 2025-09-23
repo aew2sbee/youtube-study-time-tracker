4c58a40356f8298cf22e7353f5bf5d53a7961bca
aew2sbee <aew2sbee@gmail.com>
2025-09-23 20:51:12 +0900

    fix: ユーザー情報表示のスタイルを微調整

--- src/components/FocusTimeTracker.tsx
+++ src/components/FocusTimeTracker.tsx
@@ -18,7 +18,7 @@ export default function FocusTimeTracker({ user }: { user: User[] }) {
   return (
     <div className="space-y-4 flex-1 overflow-hidden p-2">
       {user.map((user) => (
-        <div key={user.name} className="bg-black/5 rounded-lg flex items-center justify-between p-4">
+        <div key={user.name} className="bg-black/5 rounded-lg flex items-center justify-between p-3">
           <div className="flex items-center space-x-4">
             <div className="relative">
               <ImageProfile src={user.profileImageUrl} alt={user.name} />
@@ -28,7 +28,7 @@ export default function FocusTimeTracker({ user }: { user: User[] }) {
                 <ImageCrown src="/crown/silver.png" alt="crown silver" />
               ) : null}
             </div>
-            <span className="text-black font-medium truncate max-w-[160px] text-4xl">{user.name}</span>
+            <span className="text-black font-medium truncate max-w-[160px] text-3xl">{user.name}</span>
           </div>
 
           <div className="text-black flex items-center text-4xl">
@@ -37,7 +37,7 @@ export default function FocusTimeTracker({ user }: { user: User[] }) {
             ) : user.timeSec > 0 ? (
               <span className="text-gray-400 w-32 text-right mr-3">&#x25CF;</span>
             ) : null}
-            <span className="text-4xl">{calcTime(user.timeSec)}</span>
+            <span className="text-3xl">{calcTime(user.timeSec)}</span>
           </div>
         </div>
       ))}
